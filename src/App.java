d999444e06388343ec1461a5bee528b16b28169e
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2023-12-05 20:57:09 -0300

    Primeira Versão do Escopo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import avaliacao.utils.Utils;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Utils.MainEnergiaCoelho(); ;
     }
 }
